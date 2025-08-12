1b08bd951b10aa5327f4acf0ccdcef5b771cd257
uraniumcovid <yet-another-email-adress-using-proton@proton.me>
2025-08-12 23:27:41 +0200

    black back

--- quartz.config.ts
+++ quartz.config.ts
@@ -41,7 +41,7 @@ const config: QuartzConfig = {
         },
         darkMode: {
           light: "#000000",
-          lightgray: "#393639",
+          lightgray: "#000000",
           gray: "#646464",
           darkgray: "#d4d4d4",
           dark: "#ebebec",
