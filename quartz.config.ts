4179a78d999cf9df789fcf9e71fb0fe054d124c1
uraniumcovid <yet-another-email-adress-using-proton@proton.me>
2025-08-12 23:14:07 +0200

    update title

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "personal",
+    pageTitle: "personal hel(l)",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
